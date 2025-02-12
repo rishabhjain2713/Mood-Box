0dd91d138b338578beced855692eb04afd7923ab
rishabhjain2713 <jainrishabh9170@gmail.com>
2025-02-12 12:56:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     function renderWelcomeScreen() {
         root.innerHTML = `
             <div class="container">
-                <h1>Welcome Gracy! 🎭</h1>
-                <p>Let's Play! Let's See How We Can Enlighten Your Mood</p>
+                <h1>Welcome Gracy! Let's Play 🎭</h1>
+                <p>Let's see how we can enlighten your mood.</p>
                 <button id="startBtn">Let's Start</button>
             </div>
         `;
@@ -16,13 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
         root.innerHTML = `
             <div class="container">
                 <h1>What's Your Mood Today? 🤔</h1>
-                <button data-mood="Happy">😃 Happy</button>
-                <button data-mood="Angry">😡 Angry</button>
-                <button data-mood="Sad">😢 Sad</button>
-                <button data-mood="Stressed">😵‍💫 Stressed</button>
-                <button data-mood="Bored">😐 Bored</button>
-                <button data-mood="Irritated">😠 Irritated</button>
-                <button data-mood="IDontKnow">🤷‍♂️ I Don't Know</button>
+                <div class="mood-buttons">
+                    <button data-mood="Happy">😃 Happy</button>
+                    <button data-mood="Angry">😡 Angry</button>
+                    <button data-mood="Sad">😢 Sad</button>
+                    <button data-mood="Stressed">😵‍💫 Stressed</button>
+                    <button data-mood="Bored">😐 Bored</button>
+                    <button data-mood="Irritated">😠 Irritated</button>
+                    <button data-mood="IDontKnow">🤷‍♂️ I Don't Know</button>
+                </div>
             </div>
         `;
 
