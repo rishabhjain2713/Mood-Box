ad3b7649bf67865dbf25231363d98f2de3a3bd63
rishabhjain2713 <jainrishabh9170@gmail.com>
2025-02-12 21:31:53 +0530

    Update mood.js

--- mood.js
+++ mood.js
@@ -37,8 +37,8 @@ document.addEventListener("DOMContentLoaded", function () {
             { text: "Watch Think School on YouTube", link: "https://www.youtube.com/c/ThinkSchool" }
         ],
         IDontKnow: [
-            { text: "Zindagi mein agar kuch banna ho, kuch haasil karna ho, kuch jeetna ho ... toh hamesha dil ki suno ... aur agar dil bhi koi jawab na de toh aankhen band karke apni maa aur papa ka naam lo ... phir dekhna har manzil paar kar jaoge, har mushkil aasaan ho jayegi ... jeet tumhari hogi, sirf tumhari ..." }
             { text: "Join Google Meet", link: "https://meet.google.com/etk-hfpd-voh" }
+            { text: "Zindagi mein agar kuch banna ho, kuch haasil karna ho, kuch jeetna ho ... toh hamesha dil ki suno ... aur agar dil bhi koi jawab na de toh aankhen band karke apni maa aur papa ka naam lo ... phir dekhna har manzil paar kar jaoge, har mushkil aasaan ho jayegi ... jeet tumhari hogi, sirf tumhari ...", link: "https://youtu.be/YW1ZZTpSIf8?si=iGKGUBMIQbAaLe4Q" }
         ]
     };
 
